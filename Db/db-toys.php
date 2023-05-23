ac1fc0f76161b297806e7dfe13e92b450a5118c1
andreaconv <andrea2convertini@gmail.com>
2023-05-24 00:29:11 +0200

    error

--- Db/db-toys.php
+++ Db/db-toys.php
@@ -6,6 +6,6 @@ $kong  = new Toys('Kong Classic');
 $kong ->setImage('https://www.thekill.it/206-large_default/gioco-gomma-naturale-kong-classic.jpg');
 $kong ->setCategory(['Cane', 'Gatto']);
 $kong ->setPrice(13.99);
-$kong ->getCharacteristics('Galleggia e rimbalza');
+$kong ->setCharacteristics('Galleggia e rimbalza');
 $kong ->setSize('8,5 cm x 10cm');
 $listProducts[] = $kong;
