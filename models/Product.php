e8f2f77a5d03d3df63ab654f1a098c9bb80c113b
andreaconv <andrea2convertini@gmail.com>
2023-05-23 19:10:14 +0200

    [add] Stack iniziale con dettagli del prodotto

---
+++ models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+class Product {
+  private $name;
+  private $image;
+  private $category;
+  private $price;
+
+  function __construct($_name){
+    $this->setName($_name);
+  }
+
+  //SETTER
+  public function setName($_name){
+    $this->name = $_name;
+  }
+  public function setImage($_image){
+    $this->image = $_image;
+  }
+  public function setCategory($_category){
+    $this->category = $_category;
+  }
+  public function setPrice($_price){
+    $this->price = $_price;
+  }
+
+  //GETTER
+  public function getName(){
+    return $this->name;
+  }
+  public function getImage(){
+    return $this->image;
+  }
+  public function getCategory(){
+    return $this->category;
+  }
+  public function getPrice(){
+    return $this->price;
+  }
+
+}
